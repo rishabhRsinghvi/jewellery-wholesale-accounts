a3305e705eacda7ca3fc685e63db3ead2d1ef257
Rishabh Singhvi <71822131+rishabhRsinghvi@users.noreply.github.com>
2024-11-13 15:57:33 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,16 @@
-//text
+// client/src/App.js
+import React, { useState } from 'react';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <div>
+      {user ? <Dashboard user={user} /> : <Login setUser={setUser} />}
+    </div>
+  );
+};
+
+export default App;
