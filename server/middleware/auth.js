7d1cefd24735cb7508318b1c2c66ba43dd13c461
Rishabh Singhvi <71822131+rishabhRsinghvi@users.noreply.github.com>
2024-11-13 16:10:28 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,1 +1,16 @@
+// server/middleware/auth.js
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
 
+const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) return res.sendStatus(403);
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
